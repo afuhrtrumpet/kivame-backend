b7ed2020667de6aca211da8dd7bb6448df0004a6
Rakesh Kumar <kumar19@illinois.edu>
2014-06-14 19:32:23 -0700

    Adding get_tagged_places()

--- data_sandbox/data_ingest/facebook_ingest.py
+++ data_sandbox/data_ingest/facebook_ingest.py
@@ -11,6 +11,7 @@ class FacebookIngest():
     def __init__(self, oauth_access_token):
         self.graph = graph = GraphAPI(oauth_access_token)
         self.user_id = self.get_user_id()
+        self.geocoder = GoogleGeocoder()
 
     def get_user_id(self):
         resp = self.graph.get('/v2.0/me?fields=id')
@@ -33,49 +34,28 @@ class FacebookIngest():
 
         return languages
 
-    def get_tagged_photo_locations(self):
-        geocoder = GoogleGeocoder()
+    def get_tagged_places(self):
+        countries = set()
 
-        locations = ()
+        resp = self.graph.get('/v2.0/' + self.user_id + '/tagged_places')
 
-        resp = self.graph.get('/v2.0/' + self.user_id + '/photos')
-        for photo in resp['data']:
-            x = photo['tags']['data'][0]['x']
-            y = photo['tags']['data'][0]['y']
-            print str(x) + ", " + str(y)
-            reverse = geocoder.get((x, y))
-            print reverse[0].formatted_address
+        for place_dict in resp["data"]:
+            reverse = self.geocoder.get((place_dict["place"]["location"]["latitude"], place_dict["place"]["location"]["longitude"]))
+            address = reverse[0].formatted_address
+            address_tokens = address.split()
+            country =  address_tokens[len(address_tokens) - 1]
+            countries.add(country)
 
-            print str(y) + ", " + str(x)
-            reverse = geocoder.get((y, x))
-            print reverse[0].formatted_address
+        return countries
 
-        return locations
-
-
-    def get_locations(self):
-        resp = self.graph.get('/v2.0/' + self.user_id + '/photos')
-        for photo in resp['data']:
-            x = photo['tags']['data'][0]['x']
-            y = photo['tags']['data'][0]['y']
-            print str(x) + ", " + str(y)
-            reverse = geocoder.get((x, y))
-            print reverse[0].formatted_address
-
-            print str(y) + ", " + str(x)
-            reverse = geocoder.get((y, x))
-            print reverse[0].formatted_address
-
-        return locations
 
 def main ():
     fi = FacebookIngest(about_me_token)
 
-    print "User Id:", fi.user_id
+    #print "User Id:", fi.user_id
     #print "Location of Residence:", fi.get_location_of_residence()
-    print "Languages:", fi.get_languages()
-
-    #print fi.get_tagged_photo_locations()
+    #print "Languages:", fi.get_languages()
+    print "Tagged Places:", fi.get_tagged_places()
 
 if __name__ == "__main__":
     main()
